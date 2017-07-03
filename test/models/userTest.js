cf9144a0c6ce326f843ceea15806e80d142759c7
bugall <bugall@sina.cn>
2017-07-03 20:01:49 +0800

    [feat] add test module

---
+++ test/models/userTest.js
@@ -0,0 +1,16 @@
+import chai from 'chai'
+import sinon from 'sinon'
+import Promise from 'q'
+import chaiPromise from 'chai-as-promised'
+chai.use(chaiPromise)
+
+const should = chai.should()
+const assert = chai.assert
+
+describe('User-Model', () => {
+	describe('getUser', () => {
+	})
+	
+	
+})
+
