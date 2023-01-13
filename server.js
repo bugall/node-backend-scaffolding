7c59da7d03c91f5213d533217495e9420900e193
litengfei <litengfei@bytedance.com>
2023-01-13 13:48:21 +0800

    remove useless import

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const serverConfig = require('./config').default.server
 const http = require('http')
-const app = require('./src/bin/app')
+// const app = require('./src/bin/app')
 // const server = http.createServer(app.callback())
 server.listen(serverConfig.port)
 server.on('listening', () => {
